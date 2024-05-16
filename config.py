c58db35132602154a348d0035e2f1f64349e869d
robin1234578 <163468843+robin1234578@users.noreply.github.com>
2024-05-17 01:19:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/synax_support")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/synax_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/trozensupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ll_A2ZQUIZ_lll")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
